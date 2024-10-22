ae48abdcf2ae80d4664de7e7a5406a86a84d4a1a
RandonSRProctor <RandyProctor@Gmail.com>
2024-10-21 22:55:52 -0400

    Basic header items.

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,24 @@ function App() {
   const [isCollapsed, setIsCollapsed] = useState<boolean>(false);
   return (
     <>
-      <div className="Background">
+      <div className="w-screen h-screen fixed -z-20">
         <div
-          className={'Headshot ' + (isCollapsed ? '_collapsed' : '_fullscreen')}
+          className={
+            'Headshot border border-green-500' +
+            (isCollapsed ? ' _collapsed' : ' _fullscreen')
+          }
           onClick={() => setIsCollapsed(state => !state)}
         ></div>
-        <h1>click to enter</h1>
+        <div className="w-full h-full bg-blue-400 ">
+          <header className="flex">
+            <div className="w-1/6 border border-white"></div>
+            <div className="w-1/6 border border-white">Develop Software</div>
+            <div className="w-1/6 border border-white">Produce Music</div>
+            <div className="w-1/6 border border-white">Build Experiences</div>
+            <div className="w-1/6 border border-white">Invest in People</div>
+            <div className="w-1/6 border border-white">Contact</div>
+          </header>
+        </div>
       </div>
     </>
   );
