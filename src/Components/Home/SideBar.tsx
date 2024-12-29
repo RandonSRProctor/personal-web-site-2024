ac90e7e7f8b7edd75af490903abb3ef397f93895
RandonSRProctor <RandyProctor@Gmail.com>
2024-12-28 22:37:54 -0500

    Sidebar updates.

--- src/Components/Home/SideBar.tsx
+++ src/Components/Home/SideBar.tsx
@@ -1,20 +1,29 @@
 export const SideBar = () => {
   return (
-    <div className="w-80 h-full">
-      <div>
-        <button>Category</button>
+    <div className="w-80 p-2 bg-sky-300 h-full">
+      <_Topic title="Resume" subTopics={['One', 'Two', 'Three']} />
+    </div>
+  );
+};
+
+type _Topic_Props = {
+  title: string;
+  subTopics: string[];
+};
+
+const _Topic = ({ title, subTopics }: _Topic_Props) => {
+  return (
+    <div>
+      <button className="p-1">{title}</button>
+      {subTopics.length > 0 ? (
         <ul className="pl-2">
-          <li>
-            <button>SubCategory</button>
-          </li>
-          <li>
-            <button>SubCategory</button>
-          </li>
-          <li>
-            <button>SubCategory</button>
-          </li>
+          {subTopics.map(subTopic => (
+            <li>
+              <button className="p-1">{subTopic}</button>
+            </li>
+          ))}
         </ul>
-      </div>
+      ) : null}
     </div>
   );
 };
--- src/Components/TimelineSite/TimelineSite.tsx
+++ src/Components/TimelineSite/TimelineSite.tsx
@@ -4,7 +4,7 @@ const reverseTimelineData = [...timelineData].reverse();
 
 export const TimelineSite = () => {
   return (
-    <div className="nothing">
+    <div className="">
       <div className="h-full w-full -z-10 flex fixed justify-center bg-blue-950">
         <div className="bg-yellow-500 h-screen w-2"></div>
       </div>
