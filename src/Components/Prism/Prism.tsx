be9accc6ee763941d365e4cb9e9fb2f1b0aaad2b
RandonSRProctor <RandyProctor@Gmail.com>
2024-12-30 23:26:18 -0500

    Squares set behind prism glass.  Looking cool.

--- src/Components/Prism/Prism.tsx
+++ src/Components/Prism/Prism.tsx
@@ -1,7 +1,5 @@
-import { LoremIpsum } from '../StandardSite/LoremIpsum';
-
 export const Prism = () => {
-  const glass = ' bg-opacity-40 backdrop-blur-sm shadow';
+  const glass = ' bg-opacity-40 backdrop-blur-sm shadow-xl';
   const all = ' ';
 
   const yellow = ' mb-4 h-60 w-40 bg-yellow-500 rounded-r-xl';
@@ -10,21 +8,20 @@ export const Prism = () => {
 
   return (
     <div>
-      <div className="w-full h-full border border-black flex flex-col fixed z-10">
+      <div className="w-full h-full flex flex-col fixed z-10">
         <div className={`YELLOW ${glass + all + yellow}`}></div>
         <div className={`CYAN ${glass + all + cyan}`}></div>
         <div className={`FUCHSIA ${glass + all + fuchsia}`}></div>
       </div>
-      <div className="w-full h-full border border-black flex flex-col fixed -z-10">
+      <div className="w-full h-full flex flex-col fixed -z-10">
         <div className={`YELLOW ${all + yellow}`}></div>
         <div className={`CYAN ${all + cyan}`}></div>
         <div className={`FUCHSIA ${all + fuchsia}`}></div>
       </div>
-      <div className="p-10">
-        <LoremIpsum className="pb-8" />
-        <LoremIpsum className="pb-8" />
-        <LoremIpsum className="pb-8" />
-        <LoremIpsum className="pb-8" />
+      <div className="p-10 w-full h-full flex flex-col">
+        <div className="mb-20 border border-black rounded h-40 w-20"></div>
+        <div className="mb-20 border border-black rounded h-24 w-20 self-end"></div>
+        <div className="border border-black rounded h-10 w-20"></div>
       </div>
     </div>
   );
