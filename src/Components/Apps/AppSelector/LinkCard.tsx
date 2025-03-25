882e1c38d3bdb3cb7b3e3c7a31d2456dfc06f29a
RandonSRProctor <RandyProctor@Gmail.com>
2025-03-25 19:03:45 -0400

    App selection is now fast click.

--- src/Components/Apps/AppSelector/LinkCard.tsx
+++ src/Components/Apps/AppSelector/LinkCard.tsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router';
+import { NavLinkFastClick } from '../../utility/NavLinkFastClick';
 
 type Props = {
   to: string;
@@ -8,12 +8,12 @@ type Props = {
 export const LinkCard = ({ to, title }: Props) => {
   return (
     <div className="pt-2">
-      <Link
+      <NavLinkFastClick
         className="flex items-center justify-center text-orange-100 border border-black inline-block rounded w-full h-40 p-2 bg-slate-600 hover:bg-slate-500 hover:underline"
         to={to}
       >
         {title}
-      </Link>
+      </NavLinkFastClick>
     </div>
   );
 };
